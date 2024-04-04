4f29f41f8c566d478a440f7b34490101a4dcbbaf
Breno Abreu <breno2601@gmail.com>
2024-04-04 14:20:12 -0300

    Chapter 7 finished! (continuation of Chapter 5 project)

--- Chapter5/blog_project/blog_project/settings.py
+++ Chapter5/blog_project/blog_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'accounts_app.apps.AccountsAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
